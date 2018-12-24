ab32aa5268ee1c4a6cbd339ec16eaf114555568b
Zac Lucarelli <chikanz@outlook.com>
2018-12-25 02:39:15 +1100

    add zucc

--- 21_stuff/balls.js
+++ 21_stuff/balls.js
@@ -1,3 +1,14 @@
+//MMM ZUCCCC
+ <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-79357523-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        
+        gtag('config', 'UA-79357523-2');
+    </script>
+
 
 // Matter.js module aliases
 var Engine = Matter.Engine,
